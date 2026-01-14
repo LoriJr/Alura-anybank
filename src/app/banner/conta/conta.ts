f38e8d7a70db4929bef18c139f00caa5941e6e1d
lorijr <jrsilva.lorivas@gmail.com>
2026-01-14 16:11:09 -0300

    adicionando formação de data e valores monetários

--- src/app/banner/conta/conta.ts
+++ src/app/banner/conta/conta.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-conta',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe ],
   templateUrl: './conta.html',
   styleUrl: './conta.css',
 })
