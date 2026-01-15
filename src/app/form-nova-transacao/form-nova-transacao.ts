7f3f36d8e3880e358bbcbad69eb14a8627b9c4a4
lorijr <jrsilva.lorivas@gmail.com>
2026-01-15 11:57:12 -0300

    adicionando vinculo bidirecional para valores no input

--- src/app/form-nova-transacao/form-nova-transacao.ts
+++ src/app/form-nova-transacao/form-nova-transacao.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-nova-transacao',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-nova-transacao.html',
   styleUrl: './form-nova-transacao.css',
 })
 export class FormNovaTransacao {
+  valorTransacao = "20";
+
+  aoSubmeter(){
+    console.log(this.valorTransacao)
+  }
 
 }
