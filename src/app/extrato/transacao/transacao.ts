33ce3e0e9daaf298efef17678c69019e9249caac
lorijr <jrsilva.lorivas@gmail.com>
2026-01-16 17:15:13 -0300

    adicionado componentes extrato e transacao

---
+++ src/app/extrato/transacao/transacao.ts
@@ -0,0 +1,12 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transacao',
+  imports: [CurrencyPipe],
+  templateUrl: './transacao.html',
+  styleUrl: './transacao.css',
+})
+export class Transacao {
+
+}
