d6aa23b4978d1593a84ce3dac82d1428600d63fc
lorijr <jrsilva.lorivas@gmail.com>
2026-01-16 19:04:23 -0300

    ajustando sinal de saque no extrato

--- src/app/extrato/transacao/transacao.ts
+++ src/app/extrato/transacao/transacao.ts
@@ -1,6 +1,6 @@
 import { CurrencyPipe, DatePipe } from '@angular/common';
-import { Component, input } from '@angular/core';
-import { TransacaoModel } from '../../modelos/transacao';
+import { Component, computed, input } from '@angular/core';
+import { TipoTransacao, TransacaoModel } from '../../modelos/transacao';
 
 @Component({
   selector: 'app-transacao',
@@ -10,4 +10,12 @@ import { TransacaoModel } from '../../modelos/transacao';
 })
 export class Transacao {
   transacao = input.required<TransacaoModel>();
+
+  valor = computed(()=>{
+    if(this.transacao().tipo === TipoTransacao.SAQUE){
+      return -this.transacao().valor;
+    }
+    return this.transacao().valor;
+  })
+
 }
