bb58d1c8f007bc6d7f0b0928969fef213ebc768c
lorijr <jrsilva.lorivas@gmail.com>
2026-01-21 16:21:53 -0300

    importado libs para usar MatButton e MatIcon

--- src/app/banner/conta/conta.ts
+++ src/app/banner/conta/conta.ts
@@ -1,13 +1,21 @@
 import { CurrencyPipe } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-conta',
-  imports: [CurrencyPipe ],
+  imports: [CurrencyPipe, MatButtonModule, MatIconModule],
   templateUrl: './conta.html',
   styleUrl: './conta.css',
 })
 export class Conta {
 
+  saldoVisivel: boolean = false;
   saldo = input.required<number>();
+
+  saldoVisibilidade(): void{
+    this.saldoVisivel = !this.saldoVisivel;
+  }
+
 }
